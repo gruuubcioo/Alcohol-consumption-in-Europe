5429ae75912f33a5622a58dbb212feca68959d69
gruuubyy <wiktordzidziul@gmail.com>
2025-01-07 19:31:09 +0100

    Data cleaning and plots

--- data_cleaner.py
+++ data_cleaner.py
@@ -3,27 +3,28 @@ import pandas as pd
 def cleaner(data):
 
     european_countries = [
-        'Albania', 'Andorra', 'Armenia', 'Austria', 'Belarus', 'Belgium', 'Bosnia Herzegovina', 'Bulgaria', 
-        'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 
-        'Greece', 'Georgia', 'Hungary', 'Iceland', 'Ireland', 'Italy', 'Latvia', 
-        'Lithuania', 'Luxembourg', 'Malta', 'Moldova', 'Monaco', 'Montenegro', 'Netherlands', 
-        'Macedonia', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russian Federation', 'San Marino', 'Serbia', 
+        'Albania', 'Andorra', 'Armenia', 'Austria', 'Belarus', 'Belgium', 'Bosnia and Herzegovina',
+        'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France',
+        'Georgia', 'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland', 'Italy', 'Latvia', 
+        'Lithuania', 'Luxembourg', 'Malta', 'Moldova', 'Montenegro', 'Netherlands', 
+        'North Macedonia', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russia', 'Serbia', 
         'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'Switzerland', 'Turkey', 'Ukraine', 'United Kingdom'
     ]
 
-    data = pd.read_csv("./data/drinks.csv")
+    data = pd.read_csv("./data/alcohol-consumption.csv")
 
     for index, country in data['country'].items():
         if country not in european_countries:
             # Usuń wiersz, jeśli kraj nie jest w Europie
             data.drop(index, inplace=True)
 
-    for index, total_alcohol in data['total_litres_of_pure_alcohol'].items():
-        if total_alcohol <= 0.001:
-            data.drop(index, inplace=True)
+    # for index, total_alcohol in data['total_litres_of_pure_alcohol'].items():
+    #     if total_alcohol <= 0.001:
+    #         data.drop(index, inplace=True)
 
     data = data.sort_values(by='country')
     data = data.reset_index(drop=True)
+    data = data.drop(columns=['2020_projection' ,'2025_projection'])
 
     # print(data)
     # print(len(european_countries))
--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("mysarahmadbhat/alcohol-consumption")
+path = kagglehub.dataset_download("mattop/alcohol-consumption-per-capita-2016")
 
 print("Path to dataset files:", path)
